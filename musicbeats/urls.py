91718066d62dd26181f5476fd8844ffef781dd17
VICTOR4046 <lg4041@srmist.edu.in>
2022-02-02 17:50:05 +0530

    Some usual Update

--- musicbeats/urls.py
+++ musicbeats/urls.py
@@ -6,7 +6,7 @@ from django.views.static import serve
 
 urlpatterns = [
 
-    path('songs', views.songs, name='songs'),
+    path('songs/', views.songs, name='songs'),
     path('songs/<int:id>', views.songpost, name='songpost'),
     path('login', views.login, name='login'),
     path('signup', views.signup, name='signup'),
