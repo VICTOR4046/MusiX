a3af317e4c939b19e149835192619d2125c01012
VICTOR4046 <lg4041@srmist.edu.in>
2022-02-02 16:10:47 +0530

    Updating settings

--- music/settings.py
+++ music/settings.py
@@ -26,7 +26,6 @@ SECRET_KEY = 'a)bb9kz)zj)x-)cz2fh)h0mxaukx(-+p3!xhyh=qm#$!3cqd84'
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'musicbeatsx.herokuapp.com']
-CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -37,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
     'musicbeats',
 ]
 
@@ -45,7 +43,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
